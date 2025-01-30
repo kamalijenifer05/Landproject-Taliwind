e1424bcdf99a8da19f7a1b6400fb550c15d3315d
kamalijenifer05 <kamalijenifer05@gmail.com>
2025-01-30 17:48:49 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
               className="bi bi-list mr-3"
               viewBox="0 0 16 16"
             >
-              <path
+            <path
                 fillRule="evenodd"
                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
               />
